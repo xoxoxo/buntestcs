83db9d3d2ca862091805c28af683456ccb6cc350
xoxoxo <radoslaw@border3px.com>
2024-09-26 04:01:54 +0100

    bun broken

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const app = express();
 
 app.use(express.static("build"));
 app.get("/", async (req, res, next) => {
-  // await Bun.build({
-  //   entrypoints: ['./app/appGeneral.tsx'],
-  //   outdir: './out',
-  //   target: 'node', // default
-  // })
+  await Bun.build({
+    entrypoints: ['./app/appGeneral.tsx'],
+    outdir: './out',
+    target: 'node', // default
+  })
   
   try {
 
-    // const requireBundle = await import(`./out/appGeneral.js`);
-    const requireBundle = await import(`./build/main.server.js`);
+    const requireBundle = await import(`./out/appGeneral.js`);
+    // const requireBundle = await import(`./build/main.server.js`);
 
     const render = requireBundle.App.default();
 
